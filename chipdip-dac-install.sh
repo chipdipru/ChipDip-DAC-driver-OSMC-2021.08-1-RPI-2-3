b2d38c2f220584912531ae4e192d340d6a20950d
chipdip <43340836+chipdipru@users.noreply.github.com>
2021-10-01 17:51:01 +0300

    Update chipdip-dac-install.sh

--- chipdip-dac-install.sh
+++ chipdip-dac-install.sh
@@ -5,6 +5,6 @@ echo "Installing ChipDip DACs..."
 sudo cp ChipDip-DAC-driver-OSMC-2021.08-1/snd-soc-chipdip-dac-5.10.32-2-osmc.ko /lib/modules/`uname -r`/kernel/sound/soc/bcm/snd-soc-chipdip-dac.ko
 sudo cp ChipDip-DAC-driver-OSMC-2021.08-1/chipdip-dac.dtbo /boot/overlays/chipdip-dac.dtbo
 sudo depmod
-sudo sed -i -e '$adtoverlay=chipdip-dac' /boot/config-user.txt
+sudo sh -c 'echo "dtoverlay=chipdip-dac" > /boot/config-user.txt'
 
 echo "Reboot to complete installation"
